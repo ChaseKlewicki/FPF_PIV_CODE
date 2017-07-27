c50f0f8b0a99a7753c3b60c0d9184d718df2a27a
ChaseKlewicki <30506887+ChaseKlewicki@users.noreply.github.com>
2017-07-27 15:15:21 -0400

    Changed Inner.py and outer.py to Functions
    Changed the Inner and outer normalization and plotting scripts to functions. Changed the notebooks accordingly.

--- Drummonds_Scripts/PIV_Outer.py
+++ Drummonds_Scripts/PIV_Outer.py
@@ -13,76 +13,70 @@ import hotwire as hw
 #   3. Plot
 ##################################################
 #note- vel and axis are flipped to properlly calc delta
+def outer(date,num_tests,sizex,sizey,name):
+    ## Initalize variables
 
-#Set Parameters
-date = '072017'
-num_tests = 10
-sizex = 129  #Num vertical points
-sizey = 129  #num horizontal points
-## Initalize variables
+    #read in variables
+    umean = np.array(pd.read_hdf(name, 'umean_profile_avg'))*-1
+    vmean = np.array(pd.read_hdf(name, 'vmean_profile_avg'))
+    urms = np.array(pd.read_hdf(name, 'urms_profile_avg'))
+    vrms = np.array(pd.read_hdf(name, 'vrms_profile_avg'))
+    uvprime = np.array(pd.read_hdf(name, 'uvprime_profile_avg'))
+    x = np.array(pd.read_hdf(name, 'xaxis'))
+    y = np.array(pd.read_hdf(name, 'yaxis'))
+    num_tests = len(umean)
 
-#read in variables
-name = 'D:/test_072017/PIV_' + date + '.h5'
-umean = np.array(pd.read_hdf(name, 'umean_profile_avg'))*-1
-vmean = np.array(pd.read_hdf(name, 'vmean_profile_avg'))
-urms = np.array(pd.read_hdf(name, 'urms_profile_avg'))
-vrms = np.array(pd.read_hdf(name, 'vrms_profile_avg'))
-uvprime = np.array(pd.read_hdf(name, 'uvprime_profile_avg'))
-x = np.array(pd.read_hdf(name, 'xaxis'))
-y = np.array(pd.read_hdf(name, 'yaxis'))
-num_tests = len(umean)
+    ###1.  Outer Normalize #############
+    ###################################
 
-###1.  Outer Normalize #############
-###################################
+    ###2.  Outer Normalize #############
+    ###################################
 
-###2.  Outer Normalize #############
-###################################
+    ###3.  PLOTS ######################
+    ###################################
 
-###3.  PLOTS ######################
-###################################
+    #mean profiles
+    #U vs y
+    plt.figure(figsize = (10,10))
+    plt.plot(umean, y, '-xb')
+    plt.xlabel('U (m/sec)')
+    plt.ylabel('Wall Normal Position (m)')
+    plt.legend(['400rpm'], loc=0)
+    plt.show()
 
-#mean profiles
-#U vs y
-plt.figure(figsize = (10,10))
-plt.plot(umean, y, '-xb')
-plt.xlabel('U (m/sec)')
-plt.ylabel('Wall Normal Position (m)')
-plt.legend(['400rpm'], loc=0)
-plt.show()
-
-#V vs y
-plt.figure(figsize = (10,10))
-plt.plot(vmean, y, '-xr')
-plt.xlabel('V (m/sec)')
-plt.ylabel('Wall Normal Position (m)')
-plt.legend(['400rpm'], loc=0)
-plt.show()
+    #V vs y
+    plt.figure(figsize = (10,10))
+    plt.plot(vmean, y, '-xr')
+    plt.xlabel('V (m/sec)')
+    plt.ylabel('Wall Normal Position (m)')
+    plt.legend(['400rpm'], loc=0)
+    plt.show()
 
 
-#urms vs y
-plt.figure(figsize = (10,10))
-plt.plot(urms, y, '-xb')
-plt.xlabel('$U_{rms}$ (m/sec)')
-plt.ylabel('Wall Normal Position (m)')
-plt.legend(['400rpm'], loc=0)
-plt.show()
+    #urms vs y
+    plt.figure(figsize = (10,10))
+    plt.plot(urms, y, '-xb')
+    plt.xlabel('$U_{rms}$ (m/sec)')
+    plt.ylabel('Wall Normal Position (m)')
+    plt.legend(['400rpm'], loc=0)
+    plt.show()
 
-#vrms vs y
-plt.figure(figsize = (10,10))
-plt.plot(vrms, y, '-xr')
-plt.xlabel('$V_{rms}$ (m/sec)')
-plt.ylabel('Wall Normal Position (m)')
-plt.legend(['400rpm'], loc=0)
-plt.show()
+    #vrms vs y
+    plt.figure(figsize = (10,10))
+    plt.plot(vrms, y, '-xr')
+    plt.xlabel('$V_{rms}$ (m/sec)')
+    plt.ylabel('Wall Normal Position (m)')
+    plt.legend(['400rpm'], loc=0)
+    plt.show()
 
-#uprime vs y
-plt.figure(figsize = (10,10))
-plt.plot(uvprime, y, '-xr')
-plt.xlabel('$u^,v^,$')
-plt.ylabel('Wall Normal Position (m)')
-plt.legend(['400rpm'], loc=0)
-plt.show()
+    #uprime vs y
+    plt.figure(figsize = (10,10))
+    plt.plot(uvprime, y, '-xr')
+    plt.xlabel('$u^,v^,$')
+    plt.ylabel('Wall Normal Position (m)')
+    plt.legend(['400rpm'], loc=0)
+    plt.show()
 
 
 
-print('Done!')
+    print('Done!')
